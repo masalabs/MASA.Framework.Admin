b341fa1a3eaf7c5aba9b56fa28d5dad495382d33
capdiem <capdiem@live.com>
2022-02-10 11:40:50 +0800

    🐛 fix: build

--- src/Services/MASA.Framework.Admin.Service.Blogs/Program.cs
+++ src/Services/MASA.Framework.Admin.Service.Blogs/Program.cs
@@ -10,11 +10,10 @@ using MASA.Contrib.Dispatcher.IntegrationEvents.EventLogs.EF;
 using Microsoft.EntityFrameworkCore;
 using MASA.Contrib.Service.MinimalAPIs;
 using MASA.Framework.Admin.Service.Blogs.Infrastructure;
-using MASA.Framework.Configuration;
-using MASA.Framework.Development.Dapr;
 using MASA.Utils.Caching.DistributedMemory.DependencyInjection;
 using MASA.Utils.Caching.Redis.DependencyInjection;
 using MASA.Utils.Caching.Redis.Models;
+using MASA.Utils.Configuration.Json;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
@@ -43,7 +42,7 @@ builder.Services.AddDaprEventBus<IntegrationEventLogService>(options =>
         .UseEventLog<BlogDbContext>();
 });
 
-//* minimal api ×¢²á
+//* minimal api ×¢ï¿½ï¿½
 var app = builder.Services.AddServices(builder);
 
 // Configure the HTTP request pipeline.

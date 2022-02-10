55f84957c994343a95b9ae4857ba1a8dab894c4f
xuyunsheng <xuyunsheng@masastack.com>
2022-02-10 15:27:06 +0800

    feat: write

---
+++ src/Services/MASA.Framework.Admin.Service.Blogs/Model/BlogInfo/Model/BlogInfoAddModel.cs
@@ -0,0 +1,36 @@
+﻿using MASA.Framework.Admin.Service.Blogs.Infrastructure.Enum;
+
+namespace MASA.Framework.Admin.Service.Blogs.Model.BlogInfo.Model;
+
+public class BlogInfoAddModel
+{
+    /// <summary>
+    /// 主键
+    /// </summary>
+    public Guid Id { get; set; } = Guid.Empty;
+
+    /// <summary>
+    /// 标题
+    /// </summary>
+    public string Title { get; set; }
+
+    /// <summary>
+    /// 类型编号
+    /// </summary>
+    public Guid TypeId { get; set; }
+
+    /// <summary>
+    /// 内容
+    /// </summary>
+    public string Content { get; set; }
+
+    /// <summary>
+    /// 是否保存为草稿
+    /// </summary>
+    public bool IsDraft { get; set; }
+
+    /// <summary>
+    /// 文章状态
+    /// </summary>
+    public StateTypes State { get; set; }
+}
---
+++ src/Web/MASA.Framework.Admin.Blog/Shared/BlogFrontLayout.razor.cs
@@ -0,0 +1,31 @@
+﻿using MASA.Blazor.Presets;
+
+namespace MASA.Framework.Admin.Blog.Shared;
+
+public partial class BlogFrontLayout
+{
+    private Message.Model _message = new();
+
+    public void Message(string content, AlertTypes type = AlertTypes.None, int timeout = 3000)
+    {
+        _message = new Message.Model
+        {
+            Visible = true,
+            Content = content,
+            Timeout = timeout,
+            Type = type
+        };
+
+        StateHasChanged();
+    }
+
+    protected override void OnInitialized()
+    {
+        GlobalConfig.OnPageModeChanged += base.StateHasChanged;
+    }
+
+    public void Dispose()
+    {
+        GlobalConfig.OnPageModeChanged -= base.StateHasChanged;
+    }
+}

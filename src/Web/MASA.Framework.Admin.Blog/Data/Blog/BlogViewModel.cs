63edbc5c8c1fdc5e55d74621a00f262dc428aee0
yesunda <yesunda@masa.lonsid.cn>
2022-02-10 15:27:18 +0800

    add home blog

---
+++ src/Web/MASA.Framework.Admin.Blog/Data/Blog/BlogViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MASA.Framework.Admin.Blog.Data.Blog
+{
+    public class BlogViewModel
+    {
+        public Guid Id { get; set; }
+        public DateTime LastModificationTime { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Type { get; set; }
+        public string Author { get; set; }
+        public int ApprovedCount { get; set; }
+        public int CommentCount { get; set; }
+        public int Visits { get; set; }
+    }
+}

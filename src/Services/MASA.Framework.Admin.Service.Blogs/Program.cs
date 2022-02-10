ea92b5d046751d297b35a9abd7d3c5e773c7c97b
songmenghui <songmenghui@masastack.com>
2022-02-10 11:11:10 +0800

    fix:删除控制器代码

--- src/Services/MASA.Framework.Admin.Service.Blogs/Program.cs
+++ src/Services/MASA.Framework.Admin.Service.Blogs/Program.cs
@@ -20,10 +20,10 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-if (builder.Environment.IsDevelopment())
-{
-    DaprStarter.Start(AppSettings.GetModel<DaprConfig>("DaprStarter"));
-}
+//if (builder.Environment.IsDevelopment())
+//{
+//    DaprStarter.Start(AppSettings.GetModel<DaprConfig>("DaprStarter"));
+//}
 
 // Add services to the container.
 

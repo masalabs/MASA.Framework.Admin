aa1fc0808e47c927fdcef37dcb21df5c858c7978
lubing <lubing@SSKJ0015>
2022-02-10 10:43:07 +0800

    feat:add service

---
+++ MASA.Framework.Admin.Service.Dictionary/Infrastructure/Entities/Dictionary.cs
@@ -0,0 +1,18 @@
+﻿namespace MASA.Framework.Admin.Service.Dictionary.Infrastructure.Entities
+{
+    public class Dictionary
+    {
+        public int Id { get; set; }
+
+        public DateTimeOffset CreateTime { get; set; } = DateTimeOffset.Now;
+
+        public string Name { get; set; } = default!;
+
+        public string Key { get; set; } = default!;
+
+        public string Value { get; set; } = default!;
+
+        public string Description { get; set; } = default!;
+
+    }
+}

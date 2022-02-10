1542344aa4a2cb842d4ca7948d14e75a2233c892
songmenghui <songmenghui@masastack.com>
2022-02-10 14:31:35 +0800

    1

--- src/Services/MASA.Framework.Admin.Service.Blogs/OpenServices/BlogTypeService.cs
+++ src/Services/MASA.Framework.Admin.Service.Blogs/OpenServices/BlogTypeService.cs
@@ -1,4 +1,5 @@
-﻿using MASA.Contrib.Service.MinimalAPIs;
+﻿using MASA.BuildingBlocks.Dispatcher.Events;
+using MASA.Contrib.Service.MinimalAPIs;
 using MASA.Framework.Admin.Service.Blogs.Application.BlogTypes.Commands;
 using MASA.Framework.Admin.Service.Blogs.Application.Commands;
 
@@ -6,28 +7,32 @@ namespace MASA.Framework.Admin.Service.Blogs.OpenServices
 {
     public class BlogTypeService : ServiceBase
     {
+        private readonly IEventBus? _eventBus;
+
         public BlogTypeService(IServiceCollection services) : base(services)
         {
+            _eventBus = this.GetService<IEventBus>();
+
+            App.MapPost("/api/blogtype/create", CreateAsync);
+            App.MapPost("/api/blogtype/update", UpdateAsync);
+            App.MapPost("/api/blogtype/remove", RemoveAsync);
         }
 
-        public BlogTypeService(IServiceCollection services, string baseUri) : base(services, baseUri)
-        {
-        }
-        
+
         public async Task CreateAsync(CreateBlogTypeCommand command)
         {
-           
-        }
-        
-        public async Task UpdateAsync(UpdateBlogTypeCommand command)
-        {
-            
+
         }
 
-        
+        public async Task UpdateAsync(UpdateBlogTypeCommand command)
+        {
+
+        }
+
+
         public async Task RemoveAsync(RemoveBolgTypeCommand command)
         {
-            
+
         }
 
     }

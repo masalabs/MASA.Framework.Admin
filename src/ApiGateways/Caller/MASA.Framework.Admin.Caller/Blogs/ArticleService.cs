f30170f9bff71cee80815f6906781dfe93c95c26
xuyunsheng <xuyunsheng@masastack.com>
2022-02-11 16:48:30 +0800

    feat： add blog home caller

--- src/ApiGateways/Caller/MASA.Framework.Admin.Caller/Blogs/ArticleService.cs
+++ src/ApiGateways/Caller/MASA.Framework.Admin.Caller/Blogs/ArticleService.cs
@@ -1,6 +1,4 @@
-using MASA.Framework.Dapr.DaprClient;
-
-namespace MASA.Framework.Admin.Caller.Blogs;
+﻿namespace MASA.Framework.Admin.Caller.Blogs;
 
 public class ArticleService
 {
@@ -12,10 +10,31 @@ public class ArticleService
         _callerProviderProvider = callerProvider;
     }
 
-    public Task<PagingResult<BlogInfoListViewModel>> GetList(GetBlogArticleOptions options)
+    public async Task<PagingResult<BlogInfoListViewModel>> GetList(GetBlogArticleOptions options)
     {
-        return _callerProviderProvider.PostAsync<GetBlogArticleOptions, PagingResult<BlogInfoListViewModel>>(
+        return await _callerProviderProvider.PostAsync<GetBlogArticleOptions, PagingResult<BlogInfoListViewModel>>(
             "/api/articles/GetList", options);
     }
 
+    /// <summary>
+    /// 博客首页列表
+    /// </summary>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public async Task<PagingResult<BlogInfoHomeListViewModel>> BlogArticleHomeAsync(GetBlogArticleHomeOptions options)
+    {
+        return await _callerProviderProvider.PostAsync<GetBlogArticleHomeOptions,
+            PagingResult<BlogInfoHomeListViewModel>>("/​api/articles/BlogArticleHome", options);
+    }
+
+    /// <summary>
+    /// 博客用户博文列表
+    /// </summary>
+    /// <param name="options"></param>
+    /// <returns></returns>
+    public async Task<PagingResult<BlogInfoListViewModel>> BlogArticleByUserAsync(GetBlogArticleUserOptions options)
+    {
+        return await _callerProviderProvider.PostAsync<GetBlogArticleUserOptions,
+            PagingResult<BlogInfoListViewModel>>("/​api/articles/BlogArticleByUser", options);
+    }
 }

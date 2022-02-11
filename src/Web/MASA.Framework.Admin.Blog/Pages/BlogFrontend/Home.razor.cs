ca53c3878f46926a1693421306b401a6dc4d432b
songmenghui <songmenghui@masastack.com>
2022-02-11 17:48:51 +0800

    fix:add

--- src/Web/MASA.Framework.Admin.Blog/Pages/BlogFrontend/Home.razor.cs
+++ src/Web/MASA.Framework.Admin.Blog/Pages/BlogFrontend/Home.razor.cs
@@ -1,5 +1,6 @@
 ﻿using MASA.Framework.Admin.Blog.Data.Blog;
 using MASA.Framework.Admin.Caller;
+using MASA.Framework.Admin.Contracts.Blogs.BlogAdvertisingPictures.Enums;
 
 namespace MASA.Framework.Admin.Blog.Pages.BlogFrontend
 {
@@ -10,7 +11,11 @@ namespace MASA.Framework.Admin.Blog.Pages.BlogFrontend
         private bool _showWrite = false;
         private CreateBlogInfoModel _options = new() { State = StateTypes.Reviewed };
         private List<(Guid, string)> _typeList = new();
-        public PagingResult<BlogInfoHomeListViewModel> Blogs { get; set; } = new PagingResult<BlogInfoHomeListViewModel>();
+
+        public PagingResult<BlogInfoHomeListViewModel> Blogs { get; set; } =
+            new PagingResult<BlogInfoHomeListViewModel>();
+
+        public List<BlogAdvertisingPicturesListViewModel> Ad { get; set; } = new();
 
 
         protected override async Task OnInitializedAsync()
@@ -19,28 +24,27 @@ namespace MASA.Framework.Admin.Blog.Pages.BlogFrontend
         }
 
 
-        [Inject]
-        protected BlogCaller BlogCaller { get; set; }
+        [Inject] protected BlogCaller BlogCaller { get; set; }
 
 
         private async Task FetchBlogs()
         {
             Blogs = await BlogCaller.ArticleService.BlogArticleHomeAsync(new GetBlogArticleHomeOptions()
             {
-                PageIndex= _page,
-                PageSize= 10
+                PageIndex = _page,
+                PageSize = 10
             });
             if (Blogs.TotalCount > 0)
             {
                 _pageCount = Convert.ToInt32(Math.Ceiling((Decimal)Blogs.TotalCount / Convert.ToDecimal(Blogs.Size)));
             }
-        
         }
 
         protected override async void OnInitialized()
         {
             //分类
-            var typesResult = await BlogCaller.BlogTypeService.PagingAsync(new GetBlogTypePagingOption() { PageIndex = 1, PageSize = int.MaxValue });
+            var typesResult = await BlogCaller.BlogTypeService.PagingAsync(new GetBlogTypePagingOption()
+                { PageIndex = 1, PageSize = int.MaxValue });
             if (typesResult.Data is not null)
             {
                 _typeList = typesResult.Data.Select(m => (m.Id, m.TypeName)).ToList();
@@ -65,6 +69,21 @@ namespace MASA.Framework.Admin.Blog.Pages.BlogFrontend
         private async Task Cancel()
         {
             _showWrite = true;
+        }
+
+        /// <summary>
+        /// 获取首页广告位
+        /// </summary>
+        private async Task GetAdAsync()
+        {
+            await BlogCaller.AdvertisingPicturesService.GetList(new()
+            {
+                Types = new()
+                {
+                    BlogAdvertisingPicturesTypes.Home,
+                    BlogAdvertisingPicturesTypes.HomeLowerRight
+                }
+            });
         }
     }
 }

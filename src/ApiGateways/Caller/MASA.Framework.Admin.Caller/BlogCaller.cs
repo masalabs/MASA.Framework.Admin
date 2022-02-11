271d7c393be1e5e562115e5571505698f0adb6d0
songmenghui <songmenghui@masastack.com>
2022-02-11 14:56:10 +0800

    fix：1

--- src/ApiGateways/Caller/MASA.Framework.Admin.Caller/BlogCaller.cs
+++ src/ApiGateways/Caller/MASA.Framework.Admin.Caller/BlogCaller.cs
@@ -7,41 +7,15 @@ namespace MASA.Framework.Admin.Caller;
 
 public class BlogCaller : HttpClientCallerBase
 {
-    //private ArticleService _articleService;
+    private ArticleService _articleService;
 
-    //protected override string BaseAddress { get; set; } = "http://localhost:18500";
+    protected override string BaseAddress { get; set; } = "http://masa.admin.services.blogs";
 
-    //public BlogCaller(IServiceProvider serviceProvider) : base(serviceProvider)
-    //{
-    //    Name = nameof(BlogCaller);
-    //}
-
-    //public ArticleService ArticleService => _articleService ?? new ArticleService(CallerProvider);
-
-
-    //private const string APP_ID = "masa-framework-admin-service-blogs";
-
-    //public IMasaHttpClient HttpClient { get; init; }
-
-    //public BlogCaller(IWebHostEnvironment env, ILogger<MasaHttpDaprClient> logger)
-    //{
-    //    HttpClient = new MasaHttpDaprClient(APP_ID, logger);
-    //    ArticleService = new ArticleService(HttpClient);
-    //}
-
-    //public ArticleService ArticleService { get; init; }
     public BlogCaller(IServiceProvider serviceProvider) : base(serviceProvider)
     {
         Name = nameof(BlogCaller);
     }
 
-    public async Task GetBrandsAsync()
-    {
-        var response = await CallerProvider.PostAsync("/api/blogtype/create", new CreateBlogTypeModel()
-        {
-            TypeName = "測試 call"
-        });
-    }
+    public ArticleService ArticleService => _articleService ?? new ArticleService(CallerProvider);
 
-    protected override string BaseAddress { get; set; } = "http://masa.admin.services.blogs";
 }
--- src/ApiGateways/Caller/MASA.Framework.Admin.Caller/Blogs/ArticleService.cs
+++ src/ApiGateways/Caller/MASA.Framework.Admin.Caller/Blogs/ArticleService.cs
@@ -4,29 +4,18 @@ namespace MASA.Framework.Admin.Caller.Blogs;
 
 public class ArticleService
 {
-    
-    //private readonly ICallerProvider _callerProviderProvider;
 
-    //public ArticleService(ICallerProvider callerProvider)
-    //{
-    //    _callerProviderProvider = callerProvider;
-    //}
+    private readonly ICallerProvider _callerProviderProvider;
 
-    //public Task<PagingResult<BlogInfoListViewModel>> GetList(GetBlogArticleOptions options)
-    //{
-    //    return _callerProviderProvider.PostAsync<GetBlogArticleOptions, PagingResult<BlogInfoListViewModel>>(
-    //        "/api/articles/GetList", options);
-    //}
-
-    private const string BASE_API = "/api/articles";
-
-    private readonly IMasaHttpClient _client;
-
-    public ArticleService(IMasaHttpClient client) => _client = client;
+    public ArticleService(ICallerProvider callerProvider)
+    {
+        _callerProviderProvider = callerProvider;
+    }
 
     public Task<PagingResult<BlogInfoListViewModel>> GetList(GetBlogArticleOptions options)
     {
-        return _client.PostAsync<GetBlogArticleOptions, PagingResult<BlogInfoListViewModel>>(
-                $"{BASE_API}/GetList", options);
+        return _callerProviderProvider.PostAsync<GetBlogArticleOptions, PagingResult<BlogInfoListViewModel>>(
+            "/api/articles/GetList", options);
     }
+
 }

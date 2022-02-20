2da87408807040be2df44a0754bf38c35291aaea
capdiem <capdiem@live.com>
2022-02-20 15:44:51 +0800

    🆕 feat: Add DefaultTextarea

---
+++ src/Web/MASA.Framework.Admin.Blog/Components/Default/DefaultTextarea.cs
@@ -0,0 +1,15 @@
+﻿namespace MASA.Framework.Admin.Blog.Components
+{
+    public class DefaultTextarea : MTextarea
+    {
+        public override async Task SetParametersAsync(ParameterView parameters)
+        {
+            Clearable = true;
+            Dense = true;
+            HideDetails = "auto";
+            Outlined = true;
+
+            await base.SetParametersAsync(parameters);
+        }
+    }
+}

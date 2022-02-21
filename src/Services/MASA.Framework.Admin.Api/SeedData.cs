1ec1d5f814c4f4e679a8ffeafb9808565e3ba3f5
wangzengxing <1625322170@qq.com>
2022-02-21 09:09:53 +0800

    add seed data

---
+++ src/Services/MASA.Framework.Admin.Api/SeedData.cs
@@ -0,0 +1,24 @@
+﻿using MASA.Framework.Admin.Service.Api.Infrastructure.Entities;
+
+namespace MASA.Framework.Admin.Api
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using var context = serviceProvider.GetService<AdminDbContext>();
+
+            if (context.Users.Any())
+            {
+                return;
+            }
+
+            context.Add(new User
+            {
+                NickName = "zhansan",
+                Password = "123456"
+            });
+            context.SaveChanges();
+        }
+    }
+}

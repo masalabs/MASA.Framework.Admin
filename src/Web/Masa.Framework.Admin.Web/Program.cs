ec9e6c9d90174d557a02b4e591210eeb0a066b4e
Li Ming <1028139084@qq.com>
2023-04-28 18:57:33 +0800

    compeleted upgrade

--- src/Web/Masa.Framework.Admin.Web/Program.cs
+++ src/Web/Masa.Framework.Admin.Web/Program.cs
@@ -32,6 +32,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+builder.WebHost.UseWebRoot("wwwroot").UseStaticWebAssets();
 
 app.UseHttpsRedirection();
 
